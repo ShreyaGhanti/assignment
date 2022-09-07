4ff18391dbc37e34f04a17499aa1c72321670b73
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-09-07 20:59:07 +0530

    Add files via upload

---
+++ 6 september assignment/Area.java
@@ -0,0 +1,8 @@
+package com.innovation.shapes;
+
+public class Area {
+	public static void main(String[] args) {
+		Circle circle2 = new Circle(10.5F);
+		Circle circle3 = new Circle(10.5F, 3.5F);
+	}
+}
